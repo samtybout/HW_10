0afd95dae7c14d97be9420b9ff8a009fb41908b1
jpyron21 <54385942+jpyron21@users.noreply.github.com>
2019-11-07 12:39:18 -0500

    Updated with question 2 and question 3 implemented

--- emissions_report.R
+++ emissions_report.R
@@ -1,6 +1,29 @@
-co2_per_cap = function(country, year, file = "data/co2_emissions_tonnes_per_person_gapminder.csv"){
+co2_per_cap = function(country, year, file){
   data = read.csv(file)
-  year = paste("X", as.character(year), sep = "")
+  year <- paste("X", as.character(year), sep = "")
+  
+  if (!(year %in% colnames(data))){
+    stop("Not a year in dataset")
+  }
+  if (!(country %in% data$country)){
+    stop("Not a country in dataset")
+  }
   emissions = data[data$country == country, year]
-  return(emissions)
+  if(is.na(emissions)){
+    print("No data for that year")
+  }
+  
+  emissions <- data[data$country == country, year]
+  quantiles <- quantile(data[,year], c(.1, .2, .3, .4), na.rm = TRUE)
+  names(quantiles) <- c("A", "B", "C", "D")
+  grade <- "F"
+  counter = 1
+  for (q in quantiles){
+    if (emissions < q){
+      grade <- names(quantiles[counter])
+      break
+    }
+    counter = counter + 1
+  }
+  print(paste("Grade:", as.character(grade), as.character(emissions), "tonnes of emissions per person per year"))
 }
